a8a08b558e0317484504b11e26f5dbe02da3b29c
dhruv-anand-aintech <dhruv.anand@ainorthstartech.com>
2023-08-09 01:07:18 +0530

    put versions on requirements

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,8 @@ from proxy_app.app import app
 from waitress import serve
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host="0.0.0.0", port=5000)
